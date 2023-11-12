8aea98f97bed38787a09ce7a0f0980dc84a63174
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2023-11-11 22:39:52 -0500

    Changes

--- P2/main.py
+++ P2/main.py
@@ -4,13 +4,12 @@ import dash_daq as daq
 import libs.emailSender as EmailSender
 import time
 
-temperature = 30
 fan_on = False
 email_sent = False
 wait_time = 300
 
 # Initial temperature values
-temp = 25
+temp = 28
 humidity = 0
 fanImg = "assets/img/on.png" if fan_on else "assets/img/off.png"
 
@@ -24,20 +23,23 @@ external_stylesheets = [
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 app.css.config.serve_locally = True
 
+# Defines Navigation Bar
 nav_bar = html.Nav(
     className="col",
     children=[
-        html.H2("Nav")
+        html.H2("IoT")
     ]
 )
 
 
+# Defines Header
 def header(title):
     return html.Header(
         children=html.H2(title)
     )
 
 
+# Defines Gauge
 def gauge(label, unit, minimum, maximum, val=0):
     return html.Div(
         className="block",
@@ -71,18 +73,18 @@ app.layout = html.Div(
                         html.Div(
                             className="col-right",
                             children=[
-                            html.Div(
-                                className="block",
-                                children=[
-                                    html.H3(f"Fan Status: {fan_on}"),
-                                    html.Img(className="block", src=fanImg)
-                                ]
-                            )
+                                html.Div(
+                                    className="block",
+                                    children=[
+                                        html.H3(f"Fan Status: {fan_on}"),
+                                        html.Img(className="block", src=fanImg)
+                                    ]
+                                )
                             ]
                         )
                     ]
                 ),
-                # Add Interval component to trigger the callback every 5 seconds
+                # Adds Interval component to trigger the callback every 5 seconds
                 dcc.Interval(
                     id='interval-component',
                     interval=5 * 1000,  # in milliseconds
@@ -90,42 +92,49 @@ app.layout = html.Div(
                 )
             ]
         ),
+        html.Script(id='alert', children="")
     ]
 )
 
 
 # Check for email response every 5 seconds
 @app.callback(
-    Output('main-content', 'children'),
+    [Output('main-content', 'children'),
+     Output('alert', 'children')],
     [Input('interval-component', 'n_intervals')]
 )
 def update(n):
     global fan_on, email_sent, wait_time
 
+    alert = ""
     # Check for email response
-    if not email_sent and n % (wait_time / 5) == 0:
-        fan_on = EmailSender.main(temperature)
+    if not email_sent and n % (wait_time / 5) == 0 and temp > 24:
+        fan_on = EmailSender.main(temp)
         email_sent = True
+        alert = "alert('Check your email to turn on fan!');"
+
+
 
     # Update fanImg based on fan_on
     fanImg = "assets/img/on.png" if fan_on else "assets/img/off.png"
 
     # Update layout
     return [
-            gauge("Temperature", "Celsius", -50, 50, temp),
-            gauge("Humidity", "%", 0, 100, humidity),
+        gauge("Temperature", "Celsius", -50, 50, temp),
+        gauge("Humidity", "%", 0, 100, humidity),
+        html.Div(
+            className="col-right",
+            children=
             html.Div(
-                className="col-right",
-                children=
-                    html.Div(
-                    className="block col",
-                        children=[
-                            html.H3(f"Fan Status: {'on' if fan_on else 'off'}"),
-                            html.Img(className="block", src=fanImg)
-                        ]
-                    )
-                )
-            ]
+                className="block col",
+                children=[
+                    html.H3(f"Fan Status: {'on' if fan_on else 'off'}"),
+                    html.Img(className="block", src=fanImg)
+                ]
+            )
+        ),
+        alert
+    ]
 
 
 # Run the app
